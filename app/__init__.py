fd65871c0a8747888ba1ad2a6a6f1e3f56faef5b
Phillip Passmore <phillipspassmore@gmail.com>
2023-11-15 21:00:07 +0000

    fixed route issue
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
-from flask import render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-    return render_template('index.html')
+from app import routes
 
 if __name__ == '__main__':
     app.run(debug=False)
